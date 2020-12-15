0bcd54132e4f21ae8c40ab1d0f057d5bbe12ab05
vince <oxovinceoxo@gmail.com>
2020-12-15 15:50:32 +0100

    travail du mardi 15/12 page1 plus 2eme fonction (9x9)

--- js/app2.js
+++ js/app2.js
@@ -3,11 +3,12 @@ function reponse(){
 	let reponse = document.getElementById("lareponse").value
 	if(reponse.length == 0){
 		alert("Merci de remplir le champs")
-	}else if(reponse == 64){
+	}else if(reponse == 81){
 		alert("bonne reponse")
-	}else if(reponse >= 64){
+	}else if(reponse >= 81){
 		alert("la réponse est plus basse")
 	}else{
 		alert("la réponse est plus haute")
 	}
 }
+
